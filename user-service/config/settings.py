81462c1993a971cca296924625b7b8b0b03b96b2
Navaneeth Sankar <navaneethsankar07@gmail.com>
2026-07-06 09:08:28 +0530

    fix: resolve SonarCloud security issues

--- user-service/config/settings.py
+++ user-service/config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,7 +73,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-import os
 
 DATABASES = {
     "default": {
