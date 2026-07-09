c946722df0f310b4684b297323c2427bef607419
Navaneeth Sankar <navaneethsankar07@gmail.com>
2026-07-09 13:33:02 +0530

    test: trigger rollback with invalid database host

--- user-service/config/settings.py
+++ user-service/config/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os 
 from pathlib import Path
-raise Exception("Rollback Test")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
